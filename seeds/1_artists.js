04a6917985c97ae1b0bcd3f630ead411eb0026af
Brice Sinkule <bsinkule@gmail.com>
2018-08-27 12:41:41 -0600

    initial commit

---
+++ seeds/1_artists.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('artists').del()
+    .then(() => {
+      return knex('artists').insert([{
+        id: 1,
+        name: 'The Beatles'
+      }, {
+        id: 2,
+        name: 'Adele'
+      }]);
+    });
+};
